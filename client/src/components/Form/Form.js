9a49aa6834f5fab544dc02d4110b50d77e47b309
Vedansh Mishra <72354616+AttractiveDog@users.noreply.github.com>
2022-02-10 01:10:08 +0530

    Initial commit

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import FileBase from "react-file-base64";
+
+import { createPost, updatePost } from "../../actions/posts";
+import useStyles from "./styles";
+
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: ""
+  });
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((message) => message._id === currentId) : null
+  );
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
+
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ title: "", message: "", tags: "", selectedFile: "" });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (currentId === 0) {
+      dispatch(createPost({ ...postData, name: user?.result?.name }));
+      clear();
+    } else {
+      dispatch(
+        updatePost(currentId, { ...postData, name: user?.result?.name })
+      );
+      clear();
+    }
+  };
+
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Please Sign In to create your own memories and like other's memories.
+        </Typography>
+      </Paper>
+    );
+  }
+
+  return <h1>Login Succesfull</h1>;
+};
+
+export default Form;
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from '../../actions/posts';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+
+const Home = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+export default Home;
